96e950ce7671f5bb73c2fdcfe6f00f6373427f4a
mohammademon393 <mdemon357ep@gmail.com>
2026-04-11 16:10:22 +0600

    install react-leaflet and show a map in the project

--- src/Pages/Coverage/Coverage.jsx
+++ src/Pages/Coverage/Coverage.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Coverage = () => {
-    return (
-        <div>
-            <h1 className='text-center text-5xl font-black'>Coverage page</h1>
-        </div>
-    );
+  const position = [23.6850, 90.3563]; // Bangladesh coordinates
+
+  return (
+    <div>
+      <h1 className="text-5xl font-black my-5">Coverage page</h1>
+
+      {/* map container */}
+      <div className="h-[500px] mt-10 border-2 border-gray-300 rounded-xl my-10">
+        <MapContainer
+          center={position}
+          zoom={8}
+          scrollWheelZoom={false}
+          className="h-full w-full"
+        >
+          <TileLayer
+            attribution="&copy; OpenStreetMap contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Coverage;
